6fb618d09a5cafe0e98f3f904c4cec6004b2b593
valentinpate <valentinp261@gmail.com>
2023-05-21 17:57:44 -0300

    js descargas

---
+++ js/app.js
@@ -0,0 +1,44 @@
+// const topsters = ["Descargas parte 1/2", "Descargas parte 3", "Descargas parte 4", "Descargas parte 5", "Spotify 1", "Spotify 2", "Spotify 3", "Spotify 4", "Spotify 5", "RYM"]
+
+// const pick = topsters[Math.floor(Math.random()*topsters.length)]
+// console.log(topsters)
+// console.log(pick)
+
+const descargasUnoDos = ["Destroy All Monsters - 1974/1976", "DJ Tron - Fucking Hardcore", "DJ Vertigo - Sonic", "Doctor Head's World Tower - Flipper's Guitar", "God - The Anatomy of Addiction", "John Zorn - Naked City", "Les Rallizes Dénudés - Heavier than a Death in the Family", "Nang Nang", "SpaceGhostPurrp - VENENO", "Spiral Tribe - Tecno Terra", "telepath - Seikan Seikou", "Third Door from the Left - Face the Firing Squad", "Throbbing Gristle - CD1", "Tommy Wright III - Runnin' N Gunnin'", "Tatsuro Yamashita - Ride on Time", "Susumu Hirasawa - Aurora", "Susumu Hirasawa - Kenja no Propeller", "3 Segundos antes Da Queda - SADQ", "Aart Zwaans - Semi Hora", "Argon - Kone Kertoo", "Bogan Wytch - Nang Hexed...", "Boredoms - Super ae", "Boris Dzaneck - Boris Dzaneck", "Café Tacvba - Revés/Yo Soy", "Cindy Lee - Age of Tenderness", "Clotta Lanzetta - Fashion x Clotta", "Corviria - Psyco Analysis", "Dr. Dre - The Chronic", "Gerry and the Holograms - Gerry and the Holograms", "Hedge Wizard - More True Than Time Thought", "Blod Besvimelse x Heroin Makes Happy Collab", "John Zorn - Cobra", "The Martian - LBH-6251876: A Red Planet Compilation", "Patricia Taxxon - Fymynob", "Rainer - Petscop Soundtrack", "Pilotredsun", "Snoop Doggy Dogg - Doggystyle", "Tiny Mouse - Tiny Adventures", "Tom Ellard - 80s Cheesecake", "Vinterriket", "Wendy Carlos - Clockwork Orange", "Staruha Mha - Fires", "猫 シ Corp. - Palm Mall Mars"]
+
+const pickUnoDos = descargasUnoDos[Math.floor(Math.random()*descargasUnoDos.length)]
+console.log("Descargas Parte 1 / 2 - - - >", pickUnoDos)
+
+const espacio = "- - - - -"
+
+console.log(espacio)
+console.log(espacio)
+
+const descargasTres = ["Neil Young - Harvest", "Aine O'Dwyer - Music For Church Cleaners", "Basking Sharks - Shark Island", "Bernd Kistenmacher - Romantic Times", "Bernd Kistenmacher - Wake Up in the Sun", "Bizarre Ko.Ko.Ko - 00Time", "Bugger West", "Clock-DVA - Man-Amplified", "Depressive Silence - The Darkened Empires", "DJ Sprinkles - Midtown 120 Blues", "Frank Klare - Moods", "Hot Chang - The True Totality of Chang", "Ilyas Ahmed - Between Two Skies", "In Mirrors - Escape from Berlin", "Intolerance - Intolerance", "James Ferraro - Heaven's Gate" - "James Ferraro - WENQUAN", "The Klingons - Analog-Digital", "Lead Belly - Negro Sinful Songs", "Little Richard - Little Richard", "Mark Lane - Who's Really Listening?", "Mark Shreeve - Ursa Major", "Mark Shreeve - Assassin", "Michal Turtle - Music from the Living Room", "Münn - Selbstmordwald", "Park Zero - M", "Purzum - Filosofeline", "Rhys Chatham - Factor X", "Ron Berry - Where Dark Forces Meet", "Ron Berry - The Reaper", "Sacred Tapestry - Shader", "Terry Riley - In C", "A Safe Substitute - Storm Bugs", "Thoughts on Air - Mallo Yallo", "Tuluum Shimmering - These Flowers In Dawn's Twilight At Illusion Temple", "Tuluum Shimmering - In the Great Swamp You Were Conceived", "Dungeon Rap - The Introduction (Various Artists)", "Remote Viewing: Vol. 2 (Various Artists)", "RadioNome (Various Artists)", "Antarctica (Various Artists)", "Velvet Acid Christ - Calling Ov the Dead", "Wrangler - A Situation", "X-Cetra - Stardust", "Xyl - Nuclear Winter", "Y Pants - Beat It Down", "Zanov - In Course of Time", "Fushitsusha - Pathetiqué", "Mariya Takeuchi - Variety", "Paul Robeson - Spirituals", "1992 Cool Runnings", "T.J. Hustler's Metaphysical Synthesized Orchestra - Age of Individualism", "Native Brazilian Music Vol. 1 (Various Artists)"]
+
+const pickTres = descargasTres[Math.floor(Math.random()*descargasTres.length)]
+console.log("Descargas Parte 3 - - - >", pickTres)
+
+console.log(espacio)
+console.log(espacio)
+
+const descargasCuatro = ["P.D. - Inweglos", "Babylon Joke - Giga", "Krankmusik - Krankmusik", "Woody Guthrie - Woody Guthrie", "Ground-Zero - Revolutionary Pekinese Opera ver 1.2.8", "No Unauthorized - Invasion", "Nomeansno - Wrong"]
+
+const pickCuatro = descargasCuatro[Math.floor(Math.random()*descargasCuatro.length)]
+console.log("Descargas Parte 4 - - - >", pickCuatro)
+
+console.log(espacio)
+console.log(espacio)
+
+const descargasCincoUno = ["6.3 - Humeur & Molecules", "3tot - Ich und du", "A Guy Called Gerald - 28 Gun Bad Boy", "Alan Hawkshaw - Frontiers of Science", "Alfonia Tims and His Flying Tigers - Future Funk / Uncut!", "Alice Coltrane - Divine Songs", "Allan Holdsworth: FLATTire: Music for a Non Existent Movie", "Atahualpa Yupanqui - El hombre, el paisaje y su canción", "Baffo Banfi - Galaxy My Dear", "Bernard C - Lieu Magique", "Black Kray - 700 Dagreez", "Brahim Yilmaz", "C.C.C.C. - Rocket Shrine", "Camel - Camel", "Camel - Stationary Traveller", "Camel - Rajaz", "Charlemagne Palestine - Four Manifestations on Six Elements", "Chêne noir - Chant pour le Delta, la Lune et le Soleil", "Die Synthetische Republik - Faktor D", "Elend - Les Tenebres du Dehors", "Eroc - Eroc", "The Fast Set - Untitled", "Fluxion - Bipolar Defect", "From Nursery to Misery C90", "Gachet - Elite Sounds", "The Gadgets - Gadgetree", "Galdur - Age of Legends", "Georges Rodi - Actual", "Good with Parents", "Haku - Na Mele A Ka", "Heroic Viking - Ragnarök", "Insiememusicadiversa - Insiememusicadiversa", "Isao Tomita - Snowflakes are Dancing", "Jason Lescalleet ft. Graham Lambkin - The Breadwinner", "Jason Lescalleet - The Pilgrim", "K. Yoshimatu - Commercial Romanticists", "K. Yoshimatu - Sepia Reminiscence", "Kallous Boys - Kick It", "Karlheinz Stockhausen - Hymnen", "Key Sounds Label - Kanon Original Soundtrack", "Kitaro - Ten Kai", "Konk - Jams", "L.A.S.'s Crime - Inanitas", "La Peste - HL 026", "Lauri Paisley - Skywords", "Lord Nelson - Ah, Ha", "VICKI LEEKX - VICKI LEEKX", "MACROBLANK", "Merci la nuit - Merci la nuit", "Michael Garrison - In the Regions of Sunreturn", "Moebius", "Mokira - Cliphop", "Moon Wheel", "Nine Circles - Nine Circles", "Nova - Terranova", "NSRD - Kunsendorfs un Osendovskis", "Omni Trio The Deepest Cut Vol. 1", "on4road - In Rainbow Roads", "Open Graves Style - Fear", "Peste Noire - La Sanie des siècles - Panégyrique de la dégénérescence", "Pink Industry - Low Technology", "Plug - Drum 'N' Bass for Papa", "Pola - Pola Meets Lyrica", "Pure - Fetor", "Raquin", "Renaissance - Renaissance", "Reynols - Polos Mosco", "runescape.wav2", "Salaried Man Club - Gray Cross", "Salaried Man Club", "Sines of Exquisite Pleasure - Music for Hospitals", "Skee Mask - Compro", "Sumo Jungle Grandeur", "The Sperm - Shh!", "Steve Roden - Light Forms", "Vangelis - Blade Runner (Esper Edition)", "Out of Standard Deutschland Vol. 1 (Various Artists)"]
+
+const descargasCincoDos = ["Berthold Weis Band / Confidentiel Périodique", "Risky Business Soundtrack", "Vivian Stanshall - Sir Henry at Rawlinson End", "Walrus", "The Wicked Farleys - Make It It", "Bacchia Neraida", "Mahk - LV", "Yuri Morozov - The Inexplicable", "Battle Garegga OST", "Joe Hisashi", "Susumu Hirasawa - PHONON LIVE", "9.0Megathrust", "Nobue Kawana", "NEWS AT 11", "Ecophony Rinne", "Sharpnel vs Project Gabbangelion"]
+
+const pickCincoUno = descargasCincoUno[Math.floor(Math.random()*descargasCincoUno.length)]
+console.log("Descargas Parte 5 [1] - - - >", pickCincoUno)
+
+const pickCincoDos = descargasCincoDos[Math.floor(Math.random()*descargasCincoDos.length)]
+console.log("Descargas Parte 5 [2] - - - >", pickCincoDos)
+
+console.log(espacio)
+console.log(espacio)
